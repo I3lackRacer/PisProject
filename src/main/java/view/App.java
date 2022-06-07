2d22591e1787f3059013ff51d2c8705b30ea239d
Tim S <tim.martin.suellner@mni.thm.de>
2022-06-07 15:14:24 +0200

    init commit

---
+++ src/main/java/view/App.java
@@ -0,0 +1,10 @@
+package view;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.runSketch(new String[]{""}, new App());
+    }
+}
